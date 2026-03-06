960545f03c7514e14be12f0c992f05a2f209a094
Iryna <irakogut23190@gmail.com>
2026-03-06 12:50:15 +0200

    make build

---
+++ index.1fee37fc.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",breakpoints:{320:{slidesPerView:1},640:{slidesPerView:2},920:{slidesPerView:3}},spaceBetween:30,loop:!1,pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.1fee37fc.js.map
